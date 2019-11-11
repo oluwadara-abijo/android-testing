ca19117e47adb78d30c4ba2fc95d5f492bfe6532
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-11-11 16:53:17 +0100

    Write tests for a normal test list

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -10,17 +10,55 @@ class StatisticsUtilsTest {
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
 
-        //Create an active task
+        // Create an active task
         val task = Task("Android Testing", "Complete the testing basics codelab",
                 false)
 
-        //Call your function
+        // Call your function
         val tasks = listOf(task)
         val result = getActiveAndCompletedStats(tasks)
 
-        //Check the result
+        // Check the result
         assertThat(result.activeTasksPercent, `is`(100f))
         assertThat(result.completedTasksPercent, `is`(0f))
+
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_oneCompletedNoActive_returnsHundredZero() {
+        // Create a completed task
+        val completedTask = Task("Android Testing", "Take unit testing codelab",
+                true)
+
+        // Call your function
+        val tasks = listOf(completedTask)
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the results
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(100f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_twoCompletedThreeActive_returnsFortySixty() {
+        // Create two completed tasks
+        val firstTask = Task("Task 1", "Task 1 description", true)
+        val secondTask = Task("Task 2", "Task 2 description", true)
+
+        // Create three active tasks
+        val thirdTask = Task("Task 3", "Task 3 description", false)
+        val fourthTask = Task("Task 4", "Task 4 description", false)
+        val fifthTask = Task("Task 5", "Task 5 description", false)
+
+        // Create task list of all five tasks
+        val tasks = listOf(firstTask, secondTask, thirdTask, fourthTask, fifthTask)
+
+        // Call your function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the results
+        assertThat(result.activeTasksPercent, `is`(60f))
+        assertThat(result.completedTasksPercent, `is`(40f))
 
     }
 }
