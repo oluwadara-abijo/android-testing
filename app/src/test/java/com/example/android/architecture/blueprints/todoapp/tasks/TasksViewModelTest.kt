0343d0b2156c1b3e9f387040c238a92af3932e90
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-11-14 10:12:51 +0100

    Make a test class for TasksViewModel

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TasksViewModelTest {
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+        //Given a fresh TasksViewModel
+
+        //When adding a new task
+
+        //Then a new task event is triggered
+    }
+}
